611b3ca49f91fd51e977b8431877e7935c3debc2
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-08-07 01:00:25 +0100

    chore: Refactor auth module and add error handlers

---
+++ 0x01-Basic_authentication/api/v1/auth/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from typing import List, TypeVar
+
+
+User = TypeVar("User")
+
+
+class Auth:
+    """The auth class"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """The require_auth method"""
+        if not path:
+            return True
+        if not excluded_paths:
+            return True
+        if f"{path}/" in excluded_paths:
+            return False
+        if path not in excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
+
+    def authorization_header(self, request=None) -> str:
+        """The authorization_header method"""
+        if request.headers is None:
+            return None
+        else:
+            request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> User:
+        """The current_user"""
+        return request
