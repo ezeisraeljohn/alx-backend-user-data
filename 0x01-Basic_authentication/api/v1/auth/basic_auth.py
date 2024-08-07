837441ccf26d31cbe7c88df0a8f9aa13ae948177
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-08-07 02:01:18 +0100

    Refactor BasicAuth class to extract base64 authorization header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,4 +9,15 @@ from .auth import Auth
 class BasicAuth(Auth):
     """This inherits from Auth"""
 
-    pass
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """Extracts the basic auth token"""
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header:
+            return None
+
+        authorization_header_split = authorization_header.split(" ")
+        if authorization_header_split[0] + " " != "Basic ":
+            return None
+        else:
+            return authorization_header_split[1]
