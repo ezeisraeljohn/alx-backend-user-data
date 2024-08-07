262760c3ceb8931ff7940b261ee11e7bd7192278
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-08-07 01:18:39 +0100

    chore: Refactor auth module, add error handlers, and remove unused files related to Basic Authentication views

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -12,6 +12,30 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+
+auth = os.environ.get("AUTH_TYPE")
+
+if auth:
+    from api.v1.auth.auth import Auth
+
+    auth = Auth()
+
+
+@app.before_request
+def before_request():
+    """The before request handler"""
+    if not auth:
+        return
+    excluded_paths = ["/api/v1/status/", "/api/v1/unauthorized/", "/api/v1/forbidden/"]
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if not auth.authorization_header(request):
+        abort(401, description="Unauthorized")
+
+    if not auth.current_user(request):
+        abort(403, description="Forbidden")
 
 
 @app.errorhandler(404)
