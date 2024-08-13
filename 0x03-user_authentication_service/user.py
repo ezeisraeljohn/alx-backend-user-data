813f21791dfe426f59a586126be85d48d89232ba
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-08-13 23:55:31 +0100

    Refactor User model to autoincrement id in 0x03-user_authentication_service

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,7 +13,7 @@ class User(Base):
     """ The class used to define the user model"""
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
