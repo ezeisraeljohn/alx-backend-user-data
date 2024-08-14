c723e1baee7c2888e7e09fb26895bd70e889a440
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-08-14 23:17:06 +0100

    Refactor DB module in 0x03-user_authentication_service

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base, User
 
@@ -38,3 +39,12 @@ class DB:
         self._session.commit()
         new_user = self.__session.query(User).filter_by(email=email).first()
         return new_user
+
+    def find_user_by(self, *args, **kwargs):
+        """ Fine a user"""
+        if kwargs:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if not user:
+                raise NoResultFound
+            else:
+                return user
