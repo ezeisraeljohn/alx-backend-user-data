27dbc5d9e1e582abb83a698ca82b9f6f0e902a5b
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-08-15 21:53:19 +0100

    Added the login logic

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 
 """ The app"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, make_response
 from flask import request
 from auth import Auth
 
@@ -28,6 +28,21 @@ def create_user():
             return jsonify({"email": f"{email}", "message": "user created"})
         except ValueError:
             return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Logs a user in"""
+    email = request.form["email"]
+    password = request.form["password"]
+    user = AUTH.valid_login(email, password)
+    if not user:
+        abort(401)
+    else:
+        session_id = AUTH.create_session(email=email)
+        response = make_response()
+        response.set_cookie("session_id", session_id)
+    return jsonify({"email": f"{email}", "message": "logged in"})
 
 
 if __name__ == "__main__":
