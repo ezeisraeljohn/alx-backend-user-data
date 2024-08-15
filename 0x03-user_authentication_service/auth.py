8566b78cf97490b88f69410b9536c3db8614f7ea
ezeisraeljohn <ezeisraeljohn@gmail.com>
2024-08-15 22:20:15 +0100

    Added changes to the auth module

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -60,3 +60,13 @@ class Auth:
             return generated_id
         except (NoResultFound, InvalidRequestError):
             return None
+
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Gets a user based on its session id"""
+        if not session_id:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except (NoResultFound, InvalidRequestError):
+            return None
